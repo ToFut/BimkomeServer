947221780043951dc268ee6cb9a99b78eba281dc
ToFut <segevhalfon1@gmail.com>
2019-09-23 15:30:39 +0300

    Added a Procfile.

--- server_side.js
+++ server_side.js
@@ -9,8 +9,14 @@ const axios = require('axios')
 const accountSid = 'ACad6097f990b6cf31446a6470848663c9';
 const authToken = 'e1d1c54265af3d0aa8760dee74c1c161';
 const client = require('twilio')(accountSid, authToken);
-
-var allowCrossDomain = function (req, res, next) {
+const bimkomeServer = 'https://europe-west1-bimkome-1553633910964.cloudfunctions.net/app';
+let axiosConfig = {
+    headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+        "Access-Control-Allow-Origin": "*",
+    }
+  };
+  var allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
@@ -26,7 +32,7 @@ server_side.use(bodyParser.json());
 server_side.get('/signUpUsers', (req, res) => {
     console.log("Connected!");
     const user = req.query;
-    axios.post('https://europe-west1-bimkome-1553633910964.cloudfunctions.net/app/users', {
+    axios.post(bimkomeServer + '/users', {
         user
     })
         .then((response) => {
@@ -58,7 +64,7 @@ server_side.post('/signUpGardens', (req, res) => {
     console.log("Connected!");
     const garden = req.body;
     console.log('postReqResults', garden)
-    axios.post('https://europe-west1-bimkome-1553633910964.cloudfunctions.net/app/gardens',
+    axios.post(bimkomeServer + '/gardens',
         garden
     )
         .then((res) => {
@@ -71,7 +77,7 @@ server_side.post('/signUpGardens', (req, res) => {
 });
 server_side.get('/getAllUsers', (req, res) => {
     console.log("Connected!");
-    axios.get('https://europe-west1-bimkome-1553633910964.cloudfunctions.net/app/users')
+    axios.get(bimkomeServer + '/users')
         .then((response) => {
             res.send(response.data)
         })
@@ -82,12 +88,49 @@ server_side.get('/getAllUsers', (req, res) => {
 });
 server_side.get('/getAllGardens', (req, res) => {
     console.log("Connected!");
-    axios.get('https://europe-west1-bimkome-1553633910964.cloudfunctions.net/app/gardens')
+    axios.get(bimkomeServer + '/gardens')
         .then((response) => {
             res.send(response.data)
         })
         .catch((error) => {
         })
+
+});
+server_side.get('/getAllCities', (req, res) => {
+    console.log("Connected!");
+    axios.get(bimkomeServer + '/cities')
+        .then((response) => {
+            res.send(response.data)
+        })
+        .catch((error) => {
+        })
+
+});
+server_side.post('/signUpGardens', (req, res) => {
+    console.log("Connected!");
+    const data = req.body;
+    console.log('postReqResults', garden)
+    axios.put(bimkomeServer + '/gardens',
+        data
+    )
+        .then((res) => {
+            console.log(res)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+
+});
+server_side.post('/updateCity', (req, res) => {
+    console.log("Connected! update city");
+    const data = req.body;      
+    console.log( Object.keys(data)[0], JSON.stringify(Object.values(data)[0]));
+    axios.put(bimkomeServer + '/cities' + '/' + Object.keys(data)[0], JSON.stringify(Object.values(data)[0]) , axiosConfig)
+    .then((res) => {
+        console.log(res.status)
+    })
+    .catch((error) => {
+    })
 
 });
 
